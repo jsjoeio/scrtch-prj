76ec918b72092a038e24fc00a51622b2a233c12c
Joe Previte <jjprevite@gmail.com>
2024-08-16 08:37:42 -0700

    fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from "react"
-import { TipTap } from "./components/TipTap"
+import React from "react";
+import { TipTap } from "./components/TipTap";
 
 const App: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="max-w-4xl w-full px-4 sm:px-0">
-        <div className="shadow-lg rounded-lg mx-2 my-2 h-[100vh] md:h-auto md:min-h-[800px] md:m-6 md:p-6">
+      <div className="max-w-4xl w-full">
+        <div className="rounded-lg mx-2 my-2 h-[100vh] md:h-auto md:min-h-[800px] md:m-6 md:p-6">
           <h1 className="text-3xl font-extrabold mb-6">
             Digital Scratch Paper
           </h1>
@@ -13,7 +13,7 @@ const App: React.FC = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
