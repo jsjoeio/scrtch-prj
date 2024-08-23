6356703a6c1215c7edffd03c6fb55375986dd3f2
Joe Previte <jjprevite@gmail.com>
2024-08-23 08:35:24 -0700

    feat: make tabs toggleable

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import React from "react";
-import { TipTap } from "./components/TipTap";
-import { FormattedDate } from "./components/FormattedDate";
+import React from "react"
+import { TipTap } from "./components/TipTap"
+import { FormattedDate } from "./components/FormattedDate"
 
 const App: React.FC = () => {
-  const today = new Date();
+  const [activeDay, setActiveDay] = React.useState<"today" | "tomorrow">(
+    "today"
+  )
 
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="max-w-4xl w-full">
         <div className="rounded-lg mx-2 my-2 h-[100vh] md:h-auto md:min-h-[800px] md:m-6 md:p-6">
-          <FormattedDate date={today} />
+          <FormattedDate activeDay={activeDay} setActiveDay={setActiveDay} />
           <TipTap />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/components/FormattedDate.tsx
+++ src/components/FormattedDate.tsx
@@ -1,13 +1,18 @@
 import React from "react"
 import { useTranslation } from "react-i18next"
-import { addDays, format, Locale } from "date-fns"
+import { addDays, format, isSameDay, Locale } from "date-fns"
 import { es, enUS } from "date-fns/locale"
 
 interface FormattedDateProps {
-  date: Date
+  activeDay: "today" | "tomorrow"
+  setActiveDay: React.Dispatch<React.SetStateAction<"today" | "tomorrow">>
 }
 
-export const FormattedDate: React.FC<FormattedDateProps> = ({ date }) => {
+export const FormattedDate: React.FC<FormattedDateProps> = ({
+  activeDay,
+  setActiveDay,
+}) => {
+  const todayDate = new Date()
   const { i18n } = useTranslation()
 
   const formats: { [langKey: string]: { locale: Locale; formatStr: string } } =
@@ -18,17 +23,28 @@ export const FormattedDate: React.FC<FormattedDateProps> = ({ date }) => {
 
   const { locale, formatStr } = formats[i18n.language] || formats.en
   const buildFormattedDate = (d: Date) => format(d, formatStr, { locale })
-  const today = buildFormattedDate(date)
-  const tomorrow = buildFormattedDate(addDays(date, 1))
+  const today = buildFormattedDate(todayDate)
+  const tomorrow_ = addDays(todayDate, 1)
+  const tomorrow = buildFormattedDate(tomorrow_)
 
   return (
-    <div className="text-sm font-light mt-6 mb-8 ml-5 text-gray-500">
-      <span className="pr-4">
+    <div className="text-sm font-light mt-6 mb-8 ml-5">
+      <span
+        onClick={() => setActiveDay("today")}
+        className={`cursor-pointer pr-4 text-gray-${
+          activeDay === "today" ? "500" : "300"
+        }`}
+      >
         {i18n.language === "es" ? `el ${today}` : today}
       </span>
       <span>|</span>
 
-      <span className="pl-4">
+      <span
+        onClick={() => setActiveDay("tomorrow")}
+        className={`cursor-pointer pl-4 text-gray-${
+          activeDay === "tomorrow" ? "500" : "300"
+        }`}
+      >
         {i18n.language === "es" ? `el ${tomorrow}` : tomorrow}
       </span>
     </div>
