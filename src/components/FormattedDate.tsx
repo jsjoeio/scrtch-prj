0ce0522593281ed2c7e9474f49e5087f5a1a9e6b
Joe Previte <jjprevite@gmail.com>
2024-08-23 08:20:25 -0700

    fix: prettier vscode settings

--- src/components/FormattedDate.tsx
+++ src/components/FormattedDate.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { useTranslation } from "react-i18next"
-import { addDays, format, Locale, subDays } from "date-fns"
+import { addDays, format, Locale } from "date-fns"
 import { es, enUS } from "date-fns/locale"
 
 interface FormattedDateProps {
